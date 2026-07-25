40317cedbafc6149a46cf7520d9a6649410f795e
xrkang121 <3091057150@qq.com>
2026-07-26 00:37:38 +0800

    Update 合并堆.cpp

--- 数据结构/合并堆.cpp
+++ 数据结构/合并堆.cpp
@@ -16,147 +16,109 @@ cmp(x, y) == true 表示 x 的优先级比 y 低。
 x < y 时，x 优先级低，因此是大根堆。
 */
 
-template <class T, int MAXN, class Compare = less<T>>
+template <class T, class Compare = less<T>>
 struct MergeHeap
 {
     struct Node
     {
-        int ls;     // 左儿子
-        int rs;     // 右儿子
-        int dis;    // 左偏树的距离
-        int siz;    // 当前子树的元素个数
-        T val;      // 当前节点保存的值
+        int ls;     // 左儿子编号
+        int rs;     // 右儿子编号
+        int dis;    // 左偏树距离
+        int siz;    // 子树元素数量
+        T val;      // 保存的数据
     };
 
     /*
-    所有相同类型的 MergeHeap 共用同一个节点池。
+    所有相同类型的合并堆共用一个动态节点池。
 
-    这样两个堆合并时，只需要合并两个根节点，
-    不需要逐个取出再插入。
+    tr[0] 表示空节点。
+    vector 会随着插入自动扩大。
     */
-    static inline Node tr[MAXN];
+    static inline vector<Node> tr = vector<Node>(1);
 
-    // 当前一共创建了多少个节点
-    static inline int cnt = 0;
-
-    // 优先级比较函数
+    // 比较函数
     static inline Compare cmp;
 
-    // 当前堆的根节点，root == 0 表示空堆
+    // 当前堆的根节点，0表示空堆
     int root = 0;
 
-    /*
-    创建一个新节点，返回新节点编号。
-
-    注意：
-    MAXN 必须大于所有堆总共执行 push 的次数。
-    被 pop 的节点不会重新利用。
-    */
-    static int newNode(T val)
+    // 创建新节点，返回节点编号
+    static int newNode(const T &val)
     {
-        cnt++;
+        tr.push_back({
+            0,      // 左儿子
+            0,      // 右儿子
+            1,      // 初始距离
+            1,      // 初始大小
+            val     // 保存的值
+        });
 
-        tr[cnt].ls = 0;
-        tr[cnt].rs = 0;
-        tr[cnt].dis = 1;
-        tr[cnt].siz = 1;
-        tr[cnt].val = val;
-
-        return cnt;
+        return tr.size() - 1;
     }
 
-    /*
-    合并以 x、y 为根的两棵左偏树，
-    返回合并后的根节点。
-
-    这是整个合并堆最核心的函数。
-    一般不需要在外面直接调用。
-    */
+    // 合并两个根节点
     static int mergeNode(int x, int y)
     {
-        // 有一个为空，直接返回另一个
+        // 一个堆为空，返回另一个
         if (x == 0 || y == 0)
         {
             return x + y;
         }
 
         /*
-        cmp(tr[x].val, tr[y].val) == true
-        表示 x 的优先级比 y 低。
-
-        所以交换 x、y，保证 x 始终是优先级更高的节点。
+        如果x的优先级比y低，就交换。
+        保证x是优先级更高的节点。
         */
         if (cmp(tr[x].val, tr[y].val))
         {
             swap(x, y);
         }
 
-        // 把 y 合并进 x 的右子树
+        // 把y合并进x的右子树
         tr[x].rs = mergeNode(tr[x].rs, y);
 
-        /*
-        左偏树要求：
-        左儿子的 dis 不能小于右儿子的 dis。
-
-        如果不满足，就交换左右儿子。
-        */
+        // 保证左儿子的距离不小于右儿子
         if (tr[tr[x].ls].dis < tr[tr[x].rs].dis)
         {
             swap(tr[x].ls, tr[x].rs);
         }
 
-        // 根据右儿子更新距离
+        // 更新距离和大小
         tr[x].dis = tr[tr[x].rs].dis + 1;
 
-        // 更新当前子树大小
         tr[x].siz = tr[tr[x].ls].siz
                   + tr[tr[x].rs].siz + 1;
 
         return x;
     }
 
-    // 判断堆是否为空
-    bool empty()
+    // 判断是否为空
+    bool empty() const
     {
         return root == 0;
     }
 
-    // 返回当前堆的元素个数
-    int size()
+    // 返回元素个数
+    int size() const
     {
         return tr[root].siz;
     }
 
-    /*
-    返回优先级最高的元素。
-
-    使用前必须保证堆不为空：
-    if (!q.empty())
-    {
-        cout << q.top();
-    }
-    */
-    T top()
+    // 返回优先级最高的元素，使用前要保证非空
+    const T &top() const
     {
         return tr[root].val;
     }
 
-    // 插入一个新元素
-    void push(T val)
+    // 插入新元素
+    void push(const T &val)
     {
-        int now = newNode(val);
-        root = mergeNode(root, now);
+        root = mergeNode(root, newNode(val));
     }
 
-    /*
-    删除优先级最高的元素。
-
-    原来的根被删除后，
-    把它的左子树和右子树合并起来作为新根。
-    */
+    // 删除优先级最高的元素
     void pop()
     {
-        // 空堆不能删除
         if (empty())
         {
             return;
@@ -166,15 +128,47 @@ struct MergeHeap
     }
 
     /*
-    把 other 合并到当前堆。
+    把other合并进当前堆。
 
-    合并之后：
-    1. 当前堆拥有两个堆的所有元素
-    2. other 变成空堆
+    合并后：
+    当前堆包含所有元素；
+    other变成空堆。
     */
     void merge(MergeHeap &other)
     {
         root = mergeNode(root, other.root);
         other.root = 0;
+    }
+
+    /*
+    清空当前堆。
+
+    这里只让当前堆变为空堆，
+    不会释放整个公共节点池。
+    */
+    void clear()
+    {
+        root = 0;
+    }
+
+    /*
+    如果大概知道元素数量，可以提前申请空间，
+    减少vector扩容次数，但不是必须调用。
+    */
+    static void reserve(int n)
+    {
+        tr.reserve(n + 1);
+    }
+
+    /*
+    清空整个公共节点池。
+
+    只能在所有同类型合并堆都不再使用时调用，
+    例如每组测试数据结束，并且之前的堆全部作废。
+    */
+    static void resetPool()
+    {
+        tr.clear();
+        tr.resize(1);
     }
 };
