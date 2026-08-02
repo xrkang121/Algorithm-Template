79e767fa0a2c21d6a4d357ae26f86f182a0065cc
xrkang121 <3091057150@qq.com>
2026-08-02 16:49:25 +0800

    Update 笛卡尔树.cpp

--- 数据结构/笛卡尔树.cpp
+++ 数据结构/笛卡尔树.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 const int MAXN = 2e5 + 5;
 
-vector<vector<int>> tree(MAXN, vector<int>());
+vector<vector<int>> tree(MAXN);
 
 signed main()
 {
@@ -20,48 +20,91 @@ signed main()
         int n, q;
         cin >> n >> q;
 
+        for (int i = 1; i <= n; i++)
+        {
+            tree[i].clear();
+        }
+
         vector<int> arr(n + 1);
+
         for (int i = 1; i <= n; i++)
         {
             cin >> arr[i];
         }
 
-        // 单调队列求笛卡尔树
+        vector<int> ls(n + 1, 0);
+        vector<int> rs(n + 1, 0);
+        vector<int> father(n + 1, 0);
+
         stack<pair<int, int>> stk;
+
         for (int i = 1; i <= n; i++)
         {
+            pair<int, int> last;
+            last.first = -1, last.second = -1;
 
-            pair<int, int> it;
-            it.first = -1, it.second = -1;
-
-            // 小根笛卡尔树
             while (!stk.empty() && stk.top().second > arr[i])
             {
-                it = stk.top();
+                last = stk.top();
                 stk.pop();
             }
 
-            if (it.first != -1)
+            /*
+            弹栈结束后，如果栈不为空，
+            那么 i 成为栈顶节点的右儿子。
+
+            注意这里使用赋值，而不是 push_back，
+            因为原来的右儿子可能需要被替换。
+            */
+            if (!stk.empty())
             {
-                tree[i].push_back(it.first);
+                rs[stk.top().first] = i;
+                father[i] = stk.top().first;
+            }
+
+            /*
+            最后一个弹出的节点成为 i 的左儿子。
+            */
+            if (last.first != 0)
+            {
+                int x = last.first;
+
+                ls[i] = x;
+                father[x] = i;
             }
 
             stk.push({i, arr[i]});
         }
 
-        pair<int, int> it;
-        if (!stk.empty())
+        /*
+        笛卡尔树全部建好以后，
+        再根据最终的左右儿子生成邻接表。
+        */
+        for (int i = 1; i <= n; i++)
         {
-            it = stk.top();
-            stk.pop();
+            if (ls[i] != 0)
+            {
+                tree[i].push_back(ls[i]);
+            }
+
+            if (rs[i] != 0)
+            {
+                tree[i].push_back(rs[i]);
+            }
         }
 
-        while (!stk.empty())
+        int root = 0;
+
+        for (int i = 1; i <= n; i++)
         {
-            tree[stk.top().first].push_back(it.first);
-            it = stk.top();
-            stk.pop();
+            if (father[i] == 0)
+            {
+                root = i;
+                break;
+            }
         }
+
+        
     }
 
     return 0;
