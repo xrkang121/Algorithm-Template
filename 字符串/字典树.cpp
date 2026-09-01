c18fedcc3ab513ec4b553b357fb4ce0fc8d54993
xrkang121 <3091057150@qq.com>
2026-09-01 12:52:21 +0800

    Update 字典树.cpp

--- 字符串/字典树.cpp
+++ 字符串/字典树.cpp
@@ -5,44 +5,58 @@ using namespace std;
 
 const int MAXN = 2e4 + 5;
 
-typedef struct Node{
+typedef struct Node
+{
     int ch[26];
     int flag; // 标记有无字符串结尾
-}Node;
+    int pass;
+    int end;
+} Node;
 
 vector<Node> trie(MAXN);
 int tot;
 
-void init(){
-    for(int i = 0;i < MAXN;i++){
-        
-        for(int j = 0;j < 26;j++){
+void init()
+{
+    for (int i = 0; i < MAXN; i++)
+    {
+
+        for (int j = 0; j < 26; j++)
+        {
             trie[i].ch[j] = 0;
         }
 
         trie[i].flag = 0;
+        trie[i].pass = 0;
+        trie[i].end = 0;
     }
 
     tot = 0;
 }
 
-void insert(string str){
+void insert(string str)
+{
     int p = 0;
 
-    for(int i = 0;i < str.size();i++){
-        int c = str[i] - 'A';
+    for (int i = 0; i < str.size(); i++)
+    {
+        int c = str[i] - 'a';
 
-        if(trie[p].ch[c] == 0){
+        if (trie[p].ch[c] == 0)
+        {
             trie[p].ch[c] = ++tot;
         }
 
         p = trie[p].ch[c];
+        trie[p].pass++;
     }
 
     trie[p].flag = 1;
+    trie[p].end++;
 }
 
-signed main(){
+signed main()
+{
     ios::sync_with_stdio(0);
     cin.tie(0), cout.tie(0);
 
@@ -52,11 +66,13 @@ signed main(){
     cin >> n;
 
     vector<string> arr(n + 1);
-    for(int i = 1;i <= n;i++){
+    for (int i = 1; i <= n; i++)
+    {
         cin >> arr[i];
     }
 
-    for(int i = 1;i <= n;i++){
+    for (int i = 1; i <= n; i++)
+    {
         insert(arr[i]);
     }
 
